6808c4deb47fea0f5e73eee8312b271745b32587
Anoreloth <93758322+Anoreloth@users.noreply.github.com>
2021-12-16 22:13:12 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace LB4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bird bird = new Bird();
+            Cuckoo cuckoo = new Cuckoo();
+            Sitter sitter = new Sitter();
+            sitter.hatchchicks();
+            cuckoo.sing();
+            bird.fly();
+            bird.layeggs();
+        }
+
+    }
+    class Bird
+    {
+
+        public void fly()
+        {
+            Console.WriteLine("*cuckoo flying*");
+        }
+        public void layeggs()
+        {
+            Console.WriteLine("*cuckoo lay eggs*");
+        }
+    }
+    class Cuckoo : Bird
+    {
+        public void fly(Bird bird)
+        {
+            bird.fly();
+        }
+
+        public void layeggs(Bird bird)
+        {
+            bird.layeggs();
+        }
+        public void sing()
+        {
+            Console.WriteLine("*cuckoo sign*");
+        }
+    }
+    class Sitter : Cuckoo
+    {
+        public void hatchchicks()
+        {
+            Console.WriteLine("*cuckoo hatchs chicks*");
+        }
+    }
+}
